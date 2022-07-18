4050e5b579ab1a48fcc120444c93b1e154e0fd16
flipott <ottphillipa@gmail.com>
2022-07-18 13:46:25 -0400

    Implement tab switching feature inside index instead of main.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+import { drawMenu } from "../src/menu.js";
+import { drawContact } from "../src/contact.js";
+
+//Draws header to be used across all pages
+function headerDraw() {
+    const content = document.getElementById("content");
+
+    const header = document.createElement('div');
+    header.className = "header";
+    content.appendChild(header);
+
+    const logo = document.createElement('div');
+    logo.className = "logo";
+    logo.innerHTML = "Matt's"
+    header.appendChild(logo);
+
+    const tabs = document.createElement('div');
+    tabs.className = "tabs";
+    header.appendChild(tabs);
+
+    const homeTab = document.createElement('button');
+    homeTab.className = "tab";
+    homeTab.id = "home";
+    homeTab.innerHTML = "Home";
+    tabs.appendChild(homeTab);
+
+    const menuTab = document.createElement('button');
+    menuTab.className = "tab";
+    menuTab.id = "menu";
+    menuTab.innerHTML = "Menu";
+    tabs.appendChild(menuTab);
+
+    const contactTab = document.createElement('button');
+    contactTab.className = "tab";
+    contactTab.id = "contact";
+    contactTab.innerHTML = "Contact";
+    tabs.appendChild(contactTab);
+}
+
+//Draws the index page
 const drawIndex = (() => {
 
     const content = document.getElementById("content");
@@ -51,6 +91,29 @@ const drawIndex = (() => {
     const locationInformation = document.createElement('div');
     locationInformation.innerHTML = "999 Nonexistent Drive<br>Detroit, MI 48201";
     locate.appendChild(locationInformation);
+});
+
+
+//Draw header and home page upon initial load
+headerDraw();
+drawIndex();
+
+//Draw menu page if menu tab is clicked
+document.getElementById("menu").addEventListener("click", function (){
+    content.children[1].remove();
+    drawMenu();
+});
+
+//Draw home page if home tab is clicked
+document.getElementById("home").addEventListener("click", function (){
+    content.children[1].remove();
+    drawIndex();
+});
+
+//Draw contact page if contact tab is cicked
+document.getElementById("contact").addEventListener("click", function (){
+    content.children[1].remove();
+    drawContact();
 });
 
 export {drawIndex};
